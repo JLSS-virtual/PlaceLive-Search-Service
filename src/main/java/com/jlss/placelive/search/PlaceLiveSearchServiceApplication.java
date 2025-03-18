9039f571ef1220d0f70ff88521f62c174d0efd35
jeet solanki <solankijeet285@gmail.com>
2025-03-19 01:07:10 +0530

    initial commit

---
+++ src/main/java/com/jlss/placelive/search/PlaceLiveSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jlss.placelive.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceLiveSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaceLiveSearchServiceApplication.class, args);
+	}
+
+}
